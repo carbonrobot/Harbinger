a95545510df64eb7ef4cb51af2d52b8e95aaa547
Charlie Brown <carbonrobot@gmail.com>
2014-07-20 17:38:57 -0500

    Add sandbox

--- source/server/routes/index.js
+++ source/server/routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res) {
     db.connect('mongodb://localhost:27017/harbinger', function (err, db) {
         if (err) { return console.dir(err); }
 
-        // no callback, fire and forget
+        // grab the log files, sort newest first
+        // TODO: limits, paging
         db.collection('logs').find().sort({ _id: -1 }).toArray(function (err, l) {
             res.render('index', {
                 logs: l
