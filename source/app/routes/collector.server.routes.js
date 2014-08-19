8b00a482c3621fdf1edd5f0e9a325c6ce39eb337
Charles Brown <charles.brown@ampf.com>
2014-08-19 13:48:40 -0500

    Colorize level tags

--- source/app/routes/collector.server.routes.js
+++ source/app/routes/collector.server.routes.js
@@ -3,5 +3,5 @@
 module.exports = function(app) {
 	var collector = require('../../app/controllers/collector');
 	app.route('/').post(collector.notify);
-	app.route('/messages').get(collector.read)
+	app.route('/messages').get(collector.read);
 };
---
+++ source/public/modules/core/directives/level.client.directives.js
@@ -0,0 +1,9 @@
+angular.module('core').directive('level', function(){
+	
+	return {
+		restrict: 'AE',
+		replace: true,
+		template: '<span class="label label-msg-{{level|lowercase}} label-mini">{{level|uppercase}}</span>',
+		scope: { level: '@value' }
+	};
+});
