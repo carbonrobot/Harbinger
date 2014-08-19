c30b4a132e5c61598c2011956d9b99127a629f86
Charles Brown <charles.brown@ampf.com>
2014-08-18 21:58:20 -0500

    Cleanup initial view render

--- source/app/controllers/core.server.controller.js
+++ source/app/controllers/core.server.controller.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('index', {
+	res.render('layout', {
 		user: req.user || null
 	});
 };
--- source/config/express.js
+++ source/config/express.js
@@ -29,9 +29,9 @@ module.exports = function(db) {
 	});
 
 	// Setting application local variables
-	app.locals.title = config.app.title;
-	app.locals.description = config.app.description;
-	app.locals.keywords = config.app.keywords;
+	//app.locals.title = config.app.title;
+	//app.locals.description = config.app.description;
+	//app.locals.keywords = config.app.keywords;
 	app.locals.jsFiles = config.getJavaScriptAssets();
 	app.locals.cssFiles = config.getCSSAssets();
 
