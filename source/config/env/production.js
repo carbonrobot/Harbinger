722be12992bb1a0ad69f7cf5f1e5a74aa9cf7024
Charlie Brown <charles.brown@ampf.com>
2014-08-26 15:22:13 -0500

    Added application filtering

--- source/config/env/production.js
+++ source/config/env/production.js
@@ -6,7 +6,8 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css'
+				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/fontawesome/css/font-awesome.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
